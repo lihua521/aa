0f1a89e26c691e89aaabe11db55f51e16212d1b0
李财宝 <1825306263@qq.com>
2019-01-17 14:55:32 +0800

    基本框架

--- main.c
+++ main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
-
+char arr[1000];
+int index = 0;
+int isMingan(char x){
+ int count = 0;
+ for(int i=0;i<index;i++){
+     if(x == arr[i]){
+         count++;
+     }
+ }
+ if(count == 0){
+     return 0;
+ }else{
+     return 1;
+ }
+}
 int main(){
 
-    printf("hello world!");
+    printf("mingancixitong\n");
+
+    while(1){
+        printf("1---tianjia\n");
+        printf("2---shanchu\n");
+        printf("3---xianshi"\n);
+        printf("4---tihuan\n");
+        printf("5---tuichu\n")
+
+        printf("jinruxitong\n");
+        int code;
+        scanf("%d",&code);
+        
+        if(code == 1){
+
+        }
+        if(code == 1){
+            
+        }
+        if(code == 1){
+            
+        }
+        if(code == 1){
+            
+        }
+        if(code == 1){
+            
+        }
+    }
 
     return 0;
 
